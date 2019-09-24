d17a8d669b937d290d67f0666b830015af7da125
William Chong <williamchong007@msn.com>
2019-09-24 19:16:32 +0800

    🎨 Add server api error handler

--- server/api/index.js
+++ server/api/index.js
@@ -8,5 +8,13 @@ const router = Router();
 
 router.use('/proxy/cosmos/', proxy(COSMOS_ENDPOINT));
 router.use('/migrate', migrate);
+router.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  const msg = (err.response && err.response.data) || err.message || err;
+  res.set('Content-Type', 'text/plain');
+  return res.status(400).send(msg);
+});
 
 export default router;
