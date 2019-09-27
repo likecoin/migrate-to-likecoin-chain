4dde40acec07262c3ac32a2855c4dcfdaf4efc8f
William Chong <williamchong007@msn.com>
2019-09-27 18:41:29 +0800

    🔧 Update config keys

--- config/config.js
+++ config/config.js
@@ -2,4 +2,4 @@
 export const ETH_LOCK_ADDRESS = '0xc111111111111111111111111111111111111111';
 // minimum amount of ERC-20 LikeCoin to migrate in wei
 export const ETH_MIN_LIKECOIN_AMOUNT = '1000000000000000000'; // 1e18 wei === 1 LikeCoin
-
+export const ETH_CONFIRMATION_NEEDED = 2;
