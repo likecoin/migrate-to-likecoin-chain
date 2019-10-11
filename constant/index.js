fcfc8de21d5452a96da3c37c6cdfb9e233e5d27b
William Chong <williamchong007@msn.com>
2019-10-11 20:17:45 +0800

    🔧 Change bigdipper url

--- constant/index.js
+++ constant/index.js
@@ -2,6 +2,6 @@ import { IS_TESTNET } from '../common/constant';
 
 export const ETHERSCAN_HOST = IS_TESTNET ? 'https://rinkeby.etherscan.io' : 'https://etherscan.io';
 
-export const BIGDIPPER_HOST = IS_TESTNET ? 'http://35.226.174.222' : '';
+export const BIGDIPPER_HOST = IS_TESTNET ? 'https://taipei.likecoin.bigdipper.live' : 'https://taipei.likecoin.bigdipper.live';
 
 export * from '../common/constant';
