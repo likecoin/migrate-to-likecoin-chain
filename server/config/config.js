e2ef50990303753b78e7e5f5766856702b37990f
Wei-Jun Hung <weijunhung@outlook.com>
2021-08-18 21:45:15 +0800

    Revert "🐛 Use rpc proxy in frontend"
    This reverts commit 359c9aa2edc23ab043e683d1fb90efe2f9c108ab.

--- server/config/config.js
+++ server/config/config.js
@@ -3,8 +3,7 @@ export const FIRESTORE_CONFIG_ROOT = '';
 
 export const ETH_CONTRACT_ADDRESS = '0x1111111111111111111111111111111111111111';
 
-export const COSMOS_LCD_ENDPOINT = 'http://LIGHT_CLIENT_ENDPOINT/';
-export const COSMOS_RPC_ENDPOINT = 'http://RPC_ENDPOINT/';
+export const COSMOS_ENDPOINT = 'http://LIGHT_CLIENT_ENDPOINT/';
 export const COSMOS_CHAIN_ID = 'CHAIN_ID';
 export const COSMOS_GAS_PRICE = 1000.0;
 export const COSMOS_DENOM = 'nanolike';
