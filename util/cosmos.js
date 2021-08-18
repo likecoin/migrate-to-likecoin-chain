8844050d427813957c1559b19cc1be43181407dd
Wei-Jun Hung <weijunhung@outlook.com>
2021-08-18 21:48:09 +0800

    Revert "🔨 Decode msg"
    This reverts commit eed64bfb458a086c02127ed43b310ce989204772.

--- util/cosmos.js
+++ util/cosmos.js
@@ -1,25 +1,21 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { StargateClient } from '@cosmjs/stargate';
-import { decodeTxRaw } from '@cosmjs/proto-signing';
-import { MsgSend } from 'cosmjs-types/cosmos/bank/v1beta1/tx';
 
 import { COSMOS_ENDPOINT } from '../server/config/config';
 import { timeout } from '../common/util/misc';
 
-export async function waitForTxToBeMined(txHash) {
-  let msg;
+export default async function waitForTxToBeMined(txHash) {
+  let tx;
   let notFoundOnce = false;
   const client = await StargateClient.connect(COSMOS_ENDPOINT);
-  while (!msg) {
+  while (!tx) {
     /* eslint-disable no-await-in-loop */
     await timeout(1000);
     try {
       const data = await client.getTx(txHash);
       if (data) {
-        const { code, tx } = data;
+        ({ tx } = data);
+        const { code } = data;
         if (code) throw new Error(code);
-        const { body } = decodeTxRaw(tx);
-        msg = MsgSend.decode(body.messages[0].value);
       }
     } catch (err) {
       // eslint-disable-next-line no-console
@@ -29,7 +25,7 @@ export async function waitForTxToBeMined(txHash) {
       await timeout(12000); // wait for 2 block + 2s
     }
   }
-  return msg;
+  return tx;
 }
 
 export default waitForTxToBeMined;
