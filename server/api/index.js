a008641c173a00cb9510fbf171f9f41e5bd8e9f0
William Chong <me@williamchong.cloud>
2021-08-19 13:01:59 +0800

    🔨 Split RPC and LCD endpoint config

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,12 @@
 import proxy from 'express-http-proxy';
 import { Router } from 'express';
-import { COSMOS_ENDPOINT } from '../config/config';
+import { COSMOS_LCD_ENDPOINT } from '../config/config';
 
 import migrate from './migrate';
 
 const router = Router();
 
-router.use('/proxy/cosmos/txs', proxy(COSMOS_ENDPOINT, {
+router.use('/proxy/cosmos/txs', proxy(COSMOS_LCD_ENDPOINT, {
   proxyReqPathResolver: (req) => {
     const parts = req.url.split('?');
     const queryString = parts[1];
